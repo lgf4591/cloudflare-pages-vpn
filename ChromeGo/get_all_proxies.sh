2d1de42a18f5e254c76e1d0efb4917e431e162a4
lgf4591 <lgf4591@outlook.com>
2023-10-09 21:04:05 +0800

    ChromeGo init

---
+++ ChromeGo/get_all_proxies.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+mkdir -p clash
+mkdir -p wireguard
+
+curl -L https://mareep.netlify.app/sub/merged_proxies_new.yaml -o all_merged_proxies.yaml
+
+
+
+
---
+++ main.sh
@@ -0,0 +1,2 @@
+#! /bin/sh
+cd ./ChromeGo && sh get_all_proxies.sh
