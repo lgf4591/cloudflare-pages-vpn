438b4934ad7c3cf522516c601bf088627a1699f6
lgf4591 <lgf4591@outlook.com>
2023-10-11 04:40:22 +0800

    test main.sh

--- main.sh
+++ main.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
-cd ./ChromeGo && sh get_all_proxies.sh
+cd ./ChromeGo && sh get_all_proxies.sh && cd ..
 
-# wget -c https://github.com/ViRb3/wgcf/releases/download/v2.2.19/wgcf_2.2.19_windows_amd64.exe -O ./warp/windows/wgcf.exe
+wget -c https://github.com/ViRb3/wgcf/releases/download/v2.2.19/wgcf_2.2.19_windows_amd64.exe -O ./warp/windows/wgcf.exe
 wget -c https://github.com/ViRb3/wgcf/releases/download/v2.2.19/wgcf_2.2.19_linux_amd64 -O ./warp/linux/wgcf
 wget -c https://github.com/ViRb3/wgcf/releases/download/v2.2.19/wgcf_2.2.19_darwin_amd64 -O ./warp/macos/wgcf
 wget -c https://github.com/ViRb3/wgcf/releases/download/v2.2.19/wgcf_2.2.19_freebsd_amd64 -O ./warp/unix/wgcf-freebsd
